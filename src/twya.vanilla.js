e126651d75add20f34f8283f2da68b6bcf80674e
Pavlo Yakymenko <world9online@gmail.com>
2026-06-13 11:38:29 +0200

    Update twya.vanilla.js

--- src/twya.vanilla.js
+++ src/twya.vanilla.js
@@ -1,8 +1,10 @@
 // twya.vanilla.js
 import initModal from './components/modal.js'
 import initDrawer from './components/drawer.js'
+import initHotkeys from './components/hotkeys.js'
 
 document.addEventListener('DOMContentLoaded', () => {
     initModal()
     initDrawer()
+    initHotkeys()
 })
