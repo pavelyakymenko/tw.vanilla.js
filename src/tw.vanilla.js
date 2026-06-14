ec7097285c959b3748ccd1d951965415dfcf7a1c
Pavlo Yakymenko <world9online@gmail.com>
2026-06-14 15:26:32 +0200

    Add tooltip initialization on DOMContentLoaded

--- src/tw.vanilla.js
+++ src/tw.vanilla.js
@@ -2,9 +2,11 @@
 import initModal from './components/modal.js'
 import initDrawer from './components/drawer.js'
 import initHotkeys from './components/hotkeys.js'
+import initTooltips from './components/tooltips.js'
 
 document.addEventListener('DOMContentLoaded', () => {
     initModal()
     initDrawer()
     initHotkeys()
+    initTooltips()
 })
