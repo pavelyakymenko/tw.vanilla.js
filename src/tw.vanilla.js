c529b498272b78d0f155d5c76af08466af83fa46
Pavlo Yakymenko <world9online@gmail.com>
2026-06-14 16:27:27 +0200

    v. 0.7
    Add new component initializations for popovers, masks, and dropdowns

--- src/tw.vanilla.js
+++ src/tw.vanilla.js
@@ -3,10 +3,16 @@ import initModal from './components/modal.js'
 import initDrawer from './components/drawer.js'
 import initHotkeys from './components/hotkeys.js'
 import initTooltips from './components/tooltips.js'
+import initPopovers from './components/popovers.js'
+import initMasks from './components/masks.js'
+import initDropdowns from './components/dropdowns.js'
 
 document.addEventListener('DOMContentLoaded', () => {
     initModal()
     initDrawer()
     initHotkeys()
     initTooltips()
+    initPopovers()
+    initMasks()
+    initDropdowns()
 })
